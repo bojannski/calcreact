056f2c5dd0650e936218b62f0603321e2a5b6d19
bojannski <bojannski@gmail.com>
2023-07-31 19:50:32 +1200

    new

--- src/contact.js
+++ src/contact.js
@@ -6,10 +6,10 @@ export default function Contact() {
                 Contact us
             </h1>
             <div className="contactspread">
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3192.4145995975014!2d174.76190707661377!3d-36.85649377993928!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6d0d4dbf845b61d9%3A0xfe6e8f48a693ffbf!2sWhitecliffe%20College%20Of%20Arts%20And%20Design!5e0!3m2!1sen!2snz!4v1682142172378!5m2!1sen!2snz" width="600" height="450" frameborder="0" style={{border:0}} allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12441032.855052749!2d164.43035431732102!3d-40.422992699793625!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6d2c200e17779687%3A0xb1d618e2756a4733!2sNew%20Zealand!5e0!3m2!1sen!2snz!4v1690789570630!5m2!1sen!2snz" width="600" height="450" frameborder="0" style={{border:0}} allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
                 <div className="info">
                     <h3>Address:</h3>
-                    <p>Whitecliffe College Of Arts And Design, Level 7/Symonds Street, Grafton, Auckland</p>
+                    <p>new zealand</p>
                     <div>
 
                     </div>
